f021d5bf0b39f52458ad6a182350cd10b056698d
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-11 15:03:15 -0300

    fix: Label AboutSocial

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -413,10 +413,10 @@ const content = {
       title: "Minha trajetória",
       subtitle:
         "Atuação técnica com acolhimento, clareza e foco na sua necessidade.",
-      labelInstagram: "Siga-nos no Instagram",
-      labelFacebook: "Siga-nos no Facebook",
-      labelLinkedin: "Siga-nos no LinkedIn",
-      labelX: "Siga-nos no X",
+      labelInstagram: "Siga-me no Instagram",
+      labelFacebook: "Siga-me no Facebook",
+      labelLinkedin: "Siga-me no LinkedIn",
+      labelX: "Siga-me no X",
       paragraph: (
         <div>
           Lílian Oliveira é Advogada-Parecerista do Ministério da Cultura desde
@@ -480,10 +480,10 @@ const content = {
         subtitle:
           "Aproveite nossas redes sociais para mantermos contato e ficar por dentro de atualizações importantes no mundo do direito.",
         paragraph: <p></p>,
-        labelInstagram: "Siga-nos no Instagram",
-        labelFacebook: "Siga-nos no Facebook",
-        labelLinkedin: "Siga-nos no LinkedIn",
-        labelX: "Siga-nos no X",
+        labelInstagram: "Siga-me no Instagram",
+        labelFacebook: "Siga-me no Facebook",
+        labelLinkedin: "Siga-me no LinkedIn",
+        labelX: "Siga-me no X",
       },
     },
     cta: {
