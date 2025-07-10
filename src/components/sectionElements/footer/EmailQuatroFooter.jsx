e7ebfaf64cf9b7bef5daa62faab36828acf6b726
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-10 10:25:42 -0300

    fix: E-mail corrigido

--- src/components/sectionElements/footer/EmailQuatroFooter.jsx
+++ src/components/sectionElements/footer/EmailQuatroFooter.jsx
@@ -9,7 +9,7 @@ function EmailQuatroFooter() {
       <div className="flex full gap-x-[12px] items-center opacity-90">
         <Mail />
 
-        <p className="text-paragraph2">{content.texts.infos.emailSecundario}</p>
+        <p className="text-paragraph2">{content.texts.infos.emailQuatro}</p>
       </div>
     </MotionDivDownToUp>
   );
