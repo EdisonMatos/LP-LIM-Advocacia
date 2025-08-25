83fc3a7373398b78b84a39323bdb6270189bcb96
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-25 15:37:33 -0300

    fix: Subir codigo

--- src/components/sectionElements/hero/Influencer.jsx
+++ src/components/sectionElements/hero/Influencer.jsx
@@ -175,7 +175,7 @@ export default function Influencer({ appDownloadButtons }) {
         </div>
         {/* desktop */}
         <div
-          className="hidden w-full bg-top bg-repeat bg-cover desktop1:flex font-mainFont h-[700px]"
+          className="hidden w-full bg-top bg-repeat bg-cover desktop1:flex font-mainFont h-[800px]"
           style={{
             backgroundImage: `url(${content.texts.hero.heroDesktop})`,
           }}
